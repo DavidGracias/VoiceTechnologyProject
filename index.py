ccb26f27e81beb5ab7541277b592f3e2d833d8b7
David Garcia <davidg0130@gmail.com>
2018-08-02 15:29:48 -0400

    debugging changes

--- index.py
+++ index.py
@@ -22,7 +22,7 @@ ask = Ask(app, "/")
 # helper functions
 def get_question(prefix=False, format = ""):
     msg = {
-        0: "If you want a specific set, say Specific... if you want to browse for a set, say Browse", #"Please say... specific... to search for a specific set, or... browse... to search among all sets on Quizlet"
+        0: "If you want to find a specific set, say Specific... otherwise, say Browse to search among all sets", #"Please say... specific... to search for a specific set, or... browse... to search among all sets on Quizlet"
         1: "What type of quiz are you looking to study off of? ",
         2: ("You want to look for a... {}... quiz, is that correct? ").format(format),
         3: "What size study set do you want? Small, Medium or Large. ",
@@ -49,7 +49,7 @@ def get_question(prefix=False, format = ""):
         msg = "Are you sure you want to restart your quiz?"
     elif  almostEqual(session.attributes["state"]%1 , .9):
         msg = "Are you sure you want to search for a new quiz?"
-
+    print( str(session.attributes["state"]) + ": " + msg )
     return ("Sorry, I'm having trouble understanding your response... " + msg) if(prefix) else msg
 
 def get_quiz_info(get):
@@ -57,6 +57,7 @@ def get_quiz_info(get):
     if(session.attributes["quizInformation"]["category"] != "" and session.attributes["quizInformation"]["length"] != ""):
         setArray = quizletObject.search_sets(session.attributes["quizInformation"]["category"], paged=False)
         if(len(setArray["sets"]) == 0):
+        print("Error encountered: " + session.attributes["errorCode"])
             session.attributes["errorCode"] = "ERROR"
             return ""
         firstSet = setArray["sets"][session.attributes["quizTryCount"]]
@@ -72,15 +73,11 @@ def get_quiz_info(get):
                 if(len(setArray) == 0):
                     session.attributes["errorCode"] = "ERROR"
                     return ""
-
+        print("Quiz #" + str(session.attributes["quizTryCount"]) + " selected")
     elif(session.attributes["quizInformation"]["username"] != "" and session.attributes["quizInformation"]["title"] != ""):
         #implement catch no sets here
-        print("run elif in get_quiz_info")
         universal = quizletObject.make_paged_request('search/universal', {'q': session.attributes["quizInformation"]["username"]})
         users = []
-        print("users list created: ", users)
-        print(type(universal))
-        print("universal['items']: ", universal[0]['items'][0]["username"])
         try:
             for current in universal['items']:
                 if current["type"] == "user":
@@ -91,21 +88,22 @@ def get_quiz_info(get):
                     users.append(item["username"])
 
         max = 0
-        print("about to do if statement")
         for x in range(1, len(users)):
             if fuzz.token_set_ratio(users[x], session.attributes["quizInformation"]["username"]) > fuzz.token_set_ratio(users[max], session.attributes["quizInformation"]["username"]):
                 max = x
         username = users[max]
-        print("Username: ", username)
         if( len(quizletObject.make_paged_request('users/' + username + '/sets')) > 0):
             setArray = quizletObject.make_paged_request('users/' + username + '/sets')[0]
         else:
+            #is there is no user with this username
             return question(get_question(prefix=True))
+        print("User " + session.attributes["quizInformation"]["username"] + " selected")
         max = 0
         for x in range(1, len(setArray)):
             if fuzz.token_set_ratio(setArray[x]["title"], session.attributes["quizInformation"]["title"]) > fuzz.token_set_ratio(setArray[max]["title"], session.attributes["quizInformation"]["title"]):
                 max = x
         firstSet = setArray[max]
+        print("Set " + setArray[x]["title"] + " selected")
     else: return question(get_question(prefix=True))
     set = quizletObject.get_set( firstSet["id"] ) #305754982
     return set[get]
@@ -117,7 +115,6 @@ def isValidQuiz(quiz):
     (not quiz["has_access"]) or
     (quiz["lang_terms"] != "en") or
     (quiz["lang_definitions"] != "en") ):
-        print(quiz["title"])
         return False
 
     if( (session.attributes["quizInformation"]["length"] == "any") or
@@ -132,7 +129,7 @@ def shuffle_cards(mixInOld=True):
         session.attributes["errorCode"] = ""
         session.attributes["state"] = 0
         return question("There are no results matching your search: " + session.attributes["quizInformation"]["category"] +". "+ get_question())
-    if( len(session.attributes["unFamiliar"]) == 0):
+    if( len(session.attributes["unFamiliar"]) == 0 ):
         session.attributes["unFamiliar"] = get_quiz_info("terms")
     elif(mixInOld):
         session.attributes["unFamiliar"].extend(session.attributes["familiar"])
@@ -142,6 +139,9 @@ def shuffle_cards(mixInOld=True):
         temp.append( session.attributes["unFamiliar"].pop(0) )
     while( len(temp) > 0):
         session.attributes["unFamiliar"].append( temp.pop( randint(0, len(temp)-1) ) )
+    print("Flashcards: ")
+    for cards in session.attributes["unFamiliar"]:
+        print(cards["term"] + ": " + cards["definition"])
 
 def almostEqual(d1, d2):
     epsilon = 10**(-5)
@@ -164,6 +164,7 @@ def instantiateQuiz(newQuiz = True):
         shuffle_cards()
     session.attributes["quizTryCount"] = 0
     session.attributes["wrongAnswers"] = 0
+    print("New quiz instantiated")
 
 def abridgify(input):
     remove = ["the", "uh", "a"]
@@ -331,7 +332,7 @@ def AnswerIntent(response):
         elif(ratio >= 65 and session.attributes["wrongAnswers"]%2 == 1):
             prefix = "You were close! Try rephrasing or altering your answer... "
         else:
-            prefix = "It looks like you got that one wrong! "
+            prefix = "That wasn't quite right! "
 
         if( ratio < 85): #the user answered wrong twice
             session.attributes["wrongAnswers"]+=1
